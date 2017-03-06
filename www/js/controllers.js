702d01bdea8a5399d8b035cd47b2a1419cd8a781
zhannett <janet@hubub.com>
2017-03-06 10:50:28 -0500

    Added advanced list functionality + some minor code updates

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,124 +1,120 @@
 angular.module('conFusion.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-
-  // Create the reserve modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/reserve.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.reserveForm = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeReserve = function() {
-    $scope.reserveForm.hide();
-  };
-
-  // Open the reserve modal
-  $scope.reserve = function() {
-    $scope.reserveForm.show();
-  };
-
-  // Perform the reserve action when the user submits the reserve form
-  $scope.doReserve = function() {
-    console.log('Doing reserve', $scope.reserveData);
-
-    // Simulate a reserve delay. Remove this and replace with your reserve
-    // code if using a reserve system
-    $timeout(function() {
-      $scope.closeReserve();
-    }, 1000);
-  };
-})
-
-  .controller('MenuController', ['$scope', 'menuFactory', 'baseUrl', function($scope, menuFactory, baseUrl) {
-    $scope.baseUrl = baseUrl;
-    $scope.tab = 1;
-    $scope.filtText = '';
-    $scope.showDetails = false;
-    $scope.showMenu = false;
-    $scope.message = "Loading ...";
-
-    menuFactory.getDishes().query(
-      function(response) {
-        $scope.dishes = response;
-        $scope.showMenu = true;
-      },
-      function(response) {
-        $scope.message = "Error: "+response.status + " " + response.statusText;
-      });
-
-    $scope.select = function(setTab) {
-      $scope.tab = setTab;
-
-      if (setTab === 2) {
-        $scope.filtText = "appetizer";
-      }
-      else if (setTab === 3) {
-        $scope.filtText = "mains";
-      }
-      else if (setTab === 4) {
-        $scope.filtText = "dessert";
-      }
-      else {
-        $scope.filtText = "";
-      }
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+  
+    // Form data for the login modal
+    $scope.loginData = {};
+  
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+  
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+      $scope.modal.hide();
+    };
+  
+    // Open the login modal
+    $scope.login = function() {
+      $scope.modal.show();
+    };
+  
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+      console.log('Doing login', $scope.loginData);
+  
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
     };
 
-    $scope.isSelected = function (checkTab) {
-      return ($scope.tab === checkTab);
+    // Create the reserve modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/reserve.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.reserveForm = modal;
+    });
+  
+    // Triggered in the login modal to close it
+    $scope.closeReserve = function() {
+      $scope.reserveForm.hide();
     };
+  
+    // Open the reserve modal
+    $scope.reserve = function() {
+      $scope.reserveForm.show();
+    };
+  
+    // Perform the reserve action when the user submits the reserve form
+    $scope.doReserve = function() {
+      console.log('Doing reserve', $scope.reserveData);
+  
+      // Simulate a reserve delay. Remove this and replace with your reserve
+      // code if using a reserve system
+      $timeout(function() {
+        $scope.closeReserve();
+      }, 1000);
+    };
+  })
 
-    $scope.toggleDetails = function() {
-      $scope.showDetails = !$scope.showDetails;
-    };
+  .controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate',
+      function($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
+          $scope.baseURL = baseURL;
+          $scope.tab = 1;
+          $scope.filtText = '';
+          $scope.showDetails = false;
+          $scope.showMenu = false;
+          $scope.message = "Loading ...";
+      
+          menuFactory.getDishes().query(
+              function(response) {
+                  $scope.dishes = response;
+                  $scope.showMenu = true;
+              },
+              function(response) {
+                  $scope.message = "Error: "+response.status + " " + response.statusText;
+              });
+      
+          $scope.select = function(setTab) {
+              $scope.tab = setTab;
+              if (setTab === 2) {
+                  $scope.filtText = "appetizer";
+              } else if (setTab === 3) {
+                  $scope.filtText = "mains";
+              } else if (setTab === 4) {
+                  $scope.filtText = "dessert";
+              } else {
+                  $scope.filtText = "";
+              }
+          };
+          $scope.isSelected = function (checkTab) {
+              return ($scope.tab === checkTab);
+          };
+          $scope.toggleDetails = function() {
+              $scope.showDetails = !$scope.showDetails;
+          };
+          $scope.addFavorite = function (index) {
+              console.log('index is ' + index);
+              favoriteFactory.addToFavorites(index);
+              $ionicListDelegate.closeOptionButtons();
+      };
   }])
 
   .controller('ContactController', ['$scope', function($scope) {
-
     $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-
-    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-
-    $scope.channels = channels;
+    $scope.channels  = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
     $scope.invalidChannelSelection = false;
 
   }])
@@ -144,10 +140,10 @@ angular.module('conFusion.controllers', [])
     };
   }])
 
-  .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseUrl',
-    function($scope, $stateParams, menuFactory, baseUrl) {
+  .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL',
+    function($scope, $stateParams, menuFactory, baseURL) {
 
-    $scope.baseUrl = baseUrl;
+    $scope.baseURL = baseURL;
     $scope.dish = {};
     $scope.showDish = false;
     $scope.message="Loading ...";
@@ -220,14 +216,54 @@ angular.module('conFusion.controllers', [])
     }
   ])
 
-  .controller('AboutController', ['$scope', 'corporateFactory', 'baseUrl', function($scope, corporateFactory, baseUrl) {
-    $scope.baseUrl = baseUrl;
-    corporateFactory.getLeaders().query(
-      function(response) {
-        $scope.leaders = response;
-      },
-      function(response) {
-        $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+  .controller('AboutController', ['$scope', 'corporateFactory', 'baseURL', function($scope, corporateFactory, baseURL) {
+      $scope.baseURL = baseURL;
+      corporateFactory.getLeaders().query(
+          function(response) {
+              $scope.leaders = response;
+          },
+          function(response) {
+              $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+          }
+      );
+  }])
+
+  .controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate',
+      function($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate ) {
+          $scope.baseURL = baseURL;
+          $scope.shouldShowDelete = false;
+          $scope.favorites = favoriteFactory.getFavorites();
+          $scope.dishes = menuFactory.getDishes().query(
+              function (response) {
+                  $scope.dishes = response;
+                  $scope.showMenu = true;
+              },
+              function (response) {
+                  $scope.message = "Error: " + response.status + " " + response.statusText;
+              }
+          );
+          console.log($scope.dishes, $scope.favorites);
+          $scope.toggleDelete = function() {
+              $scope.shouldShowDelete  = !$scope.shouldShowDelete;
+              console.log($scope.shouldShowDelete);
+          }
+          $scope.deleteFavorite = function(index) {
+              favoriteFactory.deleteFromFavorites(index);
+              $scope.shouldShowDelete = false;
+          }
       }
-    );
-  }]);
+    ])
+
+  .filter('favoriteFilter', function() {
+      return function(dishes, favorites) {
+        var out = [];
+        for (var i = 0; i < favorites.length; i++) {
+          for (var j = 0; j < dishes.length; j++) {
+              if (dishes[j].id === favorites[i].id) {
+                out.push(dishes[j]);
+              }
+          }
+        }
+        return out;
+      }
+  });
--- www/js/services.js
+++ www/js/services.js
@@ -1,23 +1,46 @@
 'use strict';
 
 angular.module('conFusion.services', ['ngResource'])
-  .constant("baseUrl", "http://localhost:3000/")
-  .service('menuFactory', ['$resource', 'baseUrl',
-    function($resource, baseUrl) {
-      this.getDishes = function() {
-        return $resource(baseUrl + 'dishes/:id', null, {'update': {method: 'PUT'}});
-      };
-      this.getPromotions = function () {
-        return $resource(baseUrl + 'promotions/:id', null, {'update': {method: 'PUT'}});
-      };
-    }
-  ])
-  .factory('corporateFactory', ['$resource', 'baseUrl',
-    function($resource, baseUrl) {
-      var corpfac = {};
-      corpfac.getLeaders = function () {
-        return $resource(baseUrl + 'leadership/:id', null, {'update': {method: 'PUT'}});
-      };
-      return corpfac;
-    }
-  ]);
+    .constant("baseURL", "http://localhost:3000/")
+    .service('menuFactory', ['$resource', 'baseURL',
+        function($resource, baseURL) {
+            this.getDishes = function() {
+                return $resource(baseURL + 'dishes/:id', null, {'update': {method: 'PUT'}});
+            };
+            this.getPromotions = function () {
+                return $resource(baseURL + 'promotions/:id', null, {'update': {method: 'PUT'}});
+            };
+        }
+    ])
+    .factory('corporateFactory', ['$resource', 'baseURL',
+        function($resource, baseURL) {
+            var corpfac = {};
+            corpfac.getLeaders = function () {
+                return $resource(baseURL + 'leadership/:id', null, {'update': {method: 'PUT'}});
+            };
+            return corpfac;
+        }
+    ])
+    .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+        var favFac = {};
+        var favorites = [];
+        favFac.addToFavorites = function (index) {
+            for (var i = 0; i < favorites.length; i++) {
+                if (favorites[i].id === index) {
+                  return;
+                }
+            }
+            favorites.push({id: index});
+        };
+        favFac.getFavorites = function() {
+            return favorites;
+        };
+        favFac.deleteFromFavorites = function(index) {
+            for (var i = 0; i < favorites.length; i++) {
+                if (favorites[i].id === index) {
+                    favorites.splice(i, 1);
+                }
+            }
+        };
+        return favFac;
+    }])
